4da7f945fff7f248f238d5c85b7cda32213df454
Najwa Nisrina <najjw14a@gmail.com>
2023-09-15 18:36:39 +0700

    template
    home.blade.php
    template.blade.php

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -13,7 +13,8 @@
     <link rel="stylesheet" href="css/styles.css" />
     <link rel="stylesheet" href="css/template.css" />
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,100,300,700" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -25,59 +26,117 @@
 <body>
 
     <!-- Navbar -->
+    <main class="flex-shrink-0">
 
-    <section class="ftco-section">
-        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="container px-5">
+                    <a class="navbar-brand" href="index.html">SELLY</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation"><span
+                            class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
+                            <li class="nav-item"><a class="nav-link" href="about.html">Penjualan</a></li>
+                            <li class="nav-item"><a class="nav-link" href="contact.html">Stok</a></li>
+                            <li class="nav-item"><a class="nav-link" href="pricing.html">Test</a></li>
+
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" id="navbarDropdownKeuangan" href="#"
+                                    role="button" data-bs-toggle="dropdown" aria-expanded="false">Keuangan</a>
+                                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownBlog">
+                                    <li><a class="dropdown-item" href="blog-home.html">Pemasukan</a></li>
+                                    <li><a class="dropdown-item" href="blog-post.html">Pengeluaran</a></li>
+                                    <li><a class="dropdown-item" href="blog-post.html">Alur Kas</a></li>
+                                </ul>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            {{-- <section class="ftco-section"> --}}
+
+            {{-- <div class="container">
             <div class="row justify-content-center">
                 <div class="col-md-6 text-center mb-5">
                     <h2 class="heading-section">AWIKWOK BANGET</h2>
                 </div>
             </div>
-        </div>
-        <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+        </div> --}}
+            {{-- <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+
             <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-bs-target="#ftco-nav"
                     aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="fa fa-bars"></span> Menu
                 </button>
                 <div class="collapse navbar-collapse" id="ftco-nav">
+
                     <ul class="navbar-nav m-auto">
+
                         <li class="nav-item active"><a href="#" class="nav-link">Home</a></li>
+
+                        <li class="nav-item"><a href="#" class="nav-link">Penjualan</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Stok</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">Test</a></li>
 
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" id="dropdown04" data-toggle="dropdown"
-                                aria-haspopup="true" aria-expanded="false">Page</a>
+                                aria-haspopup="true" aria-expanded="false">Keuangan</a>
                             <div class="dropdown-menu" aria-labelledby="dropdown04">
-                                <a class="dropdown-item" href="#">Page 1</a>
-                                <a class="dropdown-item" href="#">Page 2</a>
-                                <a class="dropdown-item" href="#">Page 3</a>
-                                <a class="dropdown-item" href="#">Page 4</a>
+                                <a class="dropdown-item" href="#">Pemasukan</a>
+                                <a class="dropdown-item" href="#">Pengeluaran</a>
+                                <a class="dropdown-item" href="#">Alur Kas</a>
                             </div>
-
                         </li>
-                        <li class="nav-item"><a href="#" class="nav-link">Work</a></li>
-                        <li class="nav-item"><a href="#" class="nav-link">Blog</a></li>
-                        <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+
                     </ul>
+
                 </div>
             </div>
-        </nav>
 
-    </section>
+            </nav>
 
-    <!-- /Navbar -->
+            </section> --}}
 
-    @yield('page_content')
+            <!-- /Navbar -->
+
+            @yield('page_content')
+
+    </main>
 
     <!-- Footer -->
+    {{-- <footer class="bg-dark py-4 mt-auto">
+        <div class="container px-5">
+            <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+                <div class="col-auto">
+                    <div class="small m-0 text-white">Copyright &copy; Your Website 2023</div>
+                </div>
+                <div class="col-auto">
+                    <a class="link-light small" href="#!">Privacy</a>
+                    <span class="text-white mx-1">&middot;</span>
+                    <a class="link-light small" href="#!">Terms</a>
+                    <span class="text-white mx-1">&middot;</span>
+                    <a class="link-light small" href="#!">Contact</a>
+                </div>
+            </div>
+        </div>
+    </footer> --}}
     <!-- /Footer -->
 
     <!-- JS -->
     <script src="js/template.js"></script>
     <script src="js/scripts.js"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js"
+        integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous">
+    </script>
 
     <script src="js/bootstrap.bundle.min.js"></script>
 
@@ -92,8 +151,7 @@
     <script defer src="https://static.cloudflareinsights.com/beacon.min.js/v8b253dfea2ab4077af8c6f58422dfbfd1689876627854"
         integrity="sha512-bjgnUKX4azu3dLTVtie9u6TKqgx29RBwfj3QXYt5EKfWM/9hPSAI/4qcV5NACjwAo8UtTeWefx6Zq5PHcMm7Tg=="
         data-cf-beacon='{"rayId":"8065f4593ae13054","token":"cd0b4b3a733644fc843ef0b185f98241","version":"2023.8.0","si":100}'
-        crossorigin="anonymous">
-    </script>
+        crossorigin="anonymous"></script>
 </body>
 
 
